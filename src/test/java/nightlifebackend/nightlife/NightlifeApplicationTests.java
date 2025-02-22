8a7209be3420862022ac87eda3d49012abc2128b
Chano <chano@correo.ugr.es>
2025-02-22 09:49:10 +0100

    Updated pom.xml #2

---
+++ src/test/java/nightlifebackend/nightlife/NightlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package nightlifebackend.nightlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
