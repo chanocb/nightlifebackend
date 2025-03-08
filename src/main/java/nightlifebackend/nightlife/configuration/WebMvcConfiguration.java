37660735bc1d37427998ecbd097d4986648fc606
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-03-08 12:49:41 +0100

    Add cors #17

---
+++ src/main/java/nightlifebackend/nightlife/configuration/WebMvcConfiguration.java
@@ -0,0 +1,12 @@
+package nightlifebackend.nightlife.configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration  implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").maxAge(3600);
+    }
+}
