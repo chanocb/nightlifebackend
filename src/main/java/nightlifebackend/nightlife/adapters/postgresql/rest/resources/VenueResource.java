67ff50a087748d2314446f9e8f5520aeafd5f051
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-03-25 10:52:13 +0100

    Fix security endpoint #21

--- src/main/java/nightlifebackend/nightlife/adapters/postgresql/rest/resources/VenueResource.java
+++ src/main/java/nightlifebackend/nightlife/adapters/postgresql/rest/resources/VenueResource.java
@@ -3,6 +3,7 @@ package nightlifebackend.nightlife.adapters.postgresql.rest.resources;
 import jakarta.validation.Valid;
 import nightlifebackend.nightlife.domain.services.VenueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class VenueResource {
         this.venueService = venueService;
     }
 
+    @PreAuthorize("hasRole('OWNER')")
     @PostMapping
     public nightlifebackend.nightlife.domain.models.Venue create(@Valid @RequestBody nightlifebackend.nightlife.domain.models.Venue venue) {
         return this.venueService.create(venue);
