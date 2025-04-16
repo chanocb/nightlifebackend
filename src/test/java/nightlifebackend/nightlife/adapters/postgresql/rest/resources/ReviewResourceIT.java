8162e4e721f155b6faea72f1b5efba30cc491085
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2025-04-16 16:48:07 +0200

    Update test ReviewResourceIT #28

--- src/test/java/nightlifebackend/nightlife/adapters/postgresql/rest/resources/ReviewResourceIT.java
+++ src/test/java/nightlifebackend/nightlife/adapters/postgresql/rest/resources/ReviewResourceIT.java
@@ -9,13 +9,11 @@ import org.springframework.web.reactive.function.BodyInserters;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 import static nightlifebackend.nightlife.adapters.postgresql.rest.resources.ReviewResource.REVIEWS;
 import static nightlifebackend.nightlife.adapters.postgresql.rest.resources.UserResource.USERS;
 import static nightlifebackend.nightlife.adapters.postgresql.rest.resources.VenueResource.VENUES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ApiTestConfig
 public class ReviewResourceIT {
@@ -26,136 +24,38 @@ public class ReviewResourceIT {
     @Autowired
     private RestClientTestService restClientTestService;
 
-    @Test
-    void testCreate() {
-        User owner = User.builder()
-                .email("owner1001@example.com")
+    private User createUser(String email, Role role) {
+        User user = User.builder()
+                .email(email)
                 .password("1234")
                 .firstName("John")
                 .lastName("Doe")
                 .phone("987654321")
                 .birthDate(LocalDate.of(1992, 3, 5))
-                .role(Role.OWNER)
+                .role(role)
                 .build();
 
         this.webTestClient
                 .post()
                 .uri(USERS)
-                .body(BodyInserters.fromValue(owner))
+                .body(BodyInserters.fromValue(user))
                 .exchange()
                 .expectStatus().isOk();
 
-        Product product1 = Product.builder()
-                .name("Jagger")
-                .price(5.0)
-                .build();
-
-        Product product2 = Product.builder()
-                .name("Tequila")
-                .price(12.0)
-                .build();
-
-        Venue venue = Venue.builder()
-                .name("example1")
-                .phone("123456789")
-                .LGTBFriendly(true)
-                .instagram("instagram")
-                .owner(owner)
-                .products(List.of(product1, product2))
-                .build();
-
-        this.restClientTestService.loginOwner(this.webTestClient)
-                .post()
-                .uri(VENUES)
-                .body(BodyInserters.fromValue(venue))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Venue.class)
-                .value(createdVenue -> {
-                    assertEquals(venue.getName(), createdVenue.getName());
-                    assertEquals(venue.getPhone(), createdVenue.getPhone());
-                    assertEquals(venue.isLGTBFriendly(), createdVenue.isLGTBFriendly());
-                    assertEquals(venue.getInstagram(), createdVenue.getInstagram());
-                    assertEquals(venue.getOwner().getEmail(), createdVenue.getOwner().getEmail());
-                    assertTrue(createdVenue.getProducts().stream()
-                            .anyMatch(p -> p.getName().equals("Jagger")));
-                    assertTrue(createdVenue.getProducts().stream()
-                            .anyMatch(p -> p.getName().equals("Tequila")));
-                });
-        EntityExchangeResult<List<Venue>> result = this.restClientTestService.loginClient(webTestClient).get().uri(VENUES + "/name/"+venue.getName()).exchange().expectStatus().isOk().expectBodyList(Venue.class).returnResult();
-        Venue venue_created = result.getResponseBody().get(0);
-        User client = User.builder()
-                .email("client1003@example.com")
-                .password("1234")
-                .firstName("John")
-                .lastName("Doe")
-                .phone("987654321")
-                .birthDate(LocalDate.of(1992, 3, 5))
-                .role(Role.CLIENT)
-                .build();
-        Review review = Review.builder()
-                .title("titulo")
-                .opinion("This is a review")
-                .rating(5)
-                .user(client)
-                .venue(venue_created)
-                .build();
-        this.restClientTestService.loginClient(this.webTestClient)
-                .post()
-                .uri(USERS)
-                .body(BodyInserters.fromValue(client))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class);
-
-        this.restClientTestService.loginClient( this.webTestClient)
-                .post()
-                .uri(REVIEWS)
-                .body(BodyInserters.fromValue(review))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Review.class)
-                .value(r -> {
-                    assertEquals("This is a review", r.getOpinion());
-                });
+        return user;
     }
 
-    @Test
-    void testFindByVenueReference() {
-        User owner = User.builder()
-                .email("owner1002@example.com")
-                .password("1234")
-                .firstName("John")
-                .lastName("Doe")
-                .phone("987654321")
-                .birthDate(LocalDate.of(1992, 3, 5))
-                .role(Role.OWNER)
-                .build();
-
-        this.webTestClient
-                .post()
-                .uri(USERS)
-                .body(BodyInserters.fromValue(owner))
-                .exchange()
-                .expectStatus().isOk();
-
-        Product product1 = Product.builder()
-                .name("Jagger")
-                .price(5.0)
-                .build();
-
-        Product product2 = Product.builder()
-                .name("Tequila")
-                .price(12.0)
-                .build();
-
+    private Venue createVenue(String name, User owner) {
         Venue venue = Venue.builder()
-                .name("example2")
+                .name(name)
                 .phone("123456789")
                 .LGTBFriendly(true)
                 .instagram("instagram")
                 .owner(owner)
-                .products(List.of(product1, product2))
+                .products(List.of(
+                        Product.builder().name("Jagger").price(5.0).build(),
+                        Product.builder().name("Tequila").price(12.0).build()
+                ))
                 .build();
 
         this.restClientTestService.loginOwner(this.webTestClient)
@@ -163,56 +63,28 @@ public class ReviewResourceIT {
                 .uri(VENUES)
                 .body(BodyInserters.fromValue(venue))
                 .exchange()
-                .expectStatus().isOk()
-                .expectBody(Venue.class)
-                .value(createdVenue -> {
-                    assertEquals(venue.getName(), createdVenue.getName());
-                    assertEquals(venue.getPhone(), createdVenue.getPhone());
-                    assertEquals(venue.isLGTBFriendly(), createdVenue.isLGTBFriendly());
-                    assertEquals(venue.getInstagram(), createdVenue.getInstagram());
-                    assertEquals(venue.getOwner().getEmail(), createdVenue.getOwner().getEmail());
-                    assertTrue(createdVenue.getProducts().stream()
-                            .anyMatch(p -> p.getName().equals("Jagger")));
-                    assertTrue(createdVenue.getProducts().stream()
-                            .anyMatch(p -> p.getName().equals("Tequila")));
-                });
-        User client = User.builder()
-                .email("client1002@example.com")
-                .password("1234")
-                .firstName("John")
-                .lastName("Doe")
-                .phone("987654321")
-                .birthDate(LocalDate.of(1992, 3, 5))
-                .role(Role.CLIENT)
-                .build();
-        this.restClientTestService.loginClient(this.webTestClient)
-                .post()
-                .uri(USERS)
-                .body(BodyInserters.fromValue(client))
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class);
+                .expectStatus().isOk();
+
+        return venue;
+    }
+
+    private Review createReview(String title, String opinion, int rating, User client, Venue venue) {
         EntityExchangeResult<List<Venue>> result = this.restClientTestService.loginClient(webTestClient).get().uri(VENUES + "/name/"+venue.getName()).exchange().expectStatus().isOk().expectBodyList(Venue.class).returnResult();
         Venue venue_created = result.getResponseBody().get(0);
         Review review = Review.builder()
-                .title("titulo")
-                .opinion("This is a review")
-                .rating(5)
+                .title(title)
+                .opinion(opinion)
+                .rating(rating)
                 .user(client)
                 .venue(venue_created)
                 .build();
-
 
         this.restClientTestService.loginClient(this.webTestClient)
                 .post()
                 .uri(REVIEWS)
                 .body(BodyInserters.fromValue(review))
                 .exchange()
-                .expectStatus().isOk()
-                .expectBody(Review.class)
-                .value(r -> {
-                    assertEquals("This is a review", r.getOpinion());
-                });
+                .expectStatus().isOk();
 
         this.restClientTestService.loginClient(this.webTestClient)
                 .get()
@@ -228,5 +100,27 @@ public class ReviewResourceIT {
                     assertEquals(client.getEmail(), reviews.get(0).getUser().getEmail());
                     assertEquals(venue_created.getReference(), reviews.get(0).getVenue().getReference());
                 });
+
+        return review;
+    }
+    @Test
+    void testCreate() {
+        User owner = createUser("owner1001@example.com", Role.OWNER);
+        Venue venue = createVenue("example1", owner);
+        User client = createUser("client1003@example.com", Role.CLIENT);
+
+        createReview("titulo", "This is a review", 5, client, venue);
+    }
+
+    @Test
+    void testFindByVenueReference() {
+        User owner = createUser("owner1002@example.com", Role.OWNER);
+        Venue venue = createVenue("example2", owner);
+        User client = createUser("client1002@example.com", Role.CLIENT);
+
+        createReview("titulo", "This is a review", 5, client, venue);
+
+
+
     }
 }
